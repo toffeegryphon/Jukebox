1ee5e06a45b7a2c6d76f913ea62f7031c30ced52
Nairen Fu <nairenf2@illinois.edu>
2021-11-03 21:15:53 -0500

    Added home screen skeleton.

---
+++ app/src/main/java/edu/illinois/cs465/jukebox/GuestSuggestionActivity.java
@@ -0,0 +1,14 @@
+package edu.illinois.cs465.jukebox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GuestSuggestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest_suggestion);
+    }
+}
--- app/src/main/java/edu/illinois/cs465/jukebox/MainActivity.java
+++ app/src/main/java/edu/illinois/cs465/jukebox/MainActivity.java
@@ -1,13 +1,46 @@
 package edu.illinois.cs465.jukebox;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean wasPartyCreated;
+    boolean hasPartyStarted;
+
+    Button buttonHost, buttonGuest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wasPartyCreated = false;
+        hasPartyStarted = false;
+
+        buttonHost = (Button) findViewById(R.id.button_host);
+        buttonGuest = (Button) findViewById(R.id.button_guest);
+
+        initListeners();
+    }
+
+    private void initListeners() {
+        buttonHost.setOnClickListener(v -> {
+            Intent goToHostCreation = new Intent(MainActivity.this, HostCreationActivity.class);
+            startActivity(goToHostCreation);
+        });
+
+        buttonGuest.setOnClickListener(v -> {
+            Intent goToGuest;
+            if (hasPartyStarted) {
+                goToGuest = new Intent(MainActivity.this, GuestPartyActivity.class);
+            } else {
+                goToGuest = new Intent(MainActivity.this, GuestSuggestionActivity.class);
+            }
+            startActivity(goToGuest);
+        });
     }
 }
